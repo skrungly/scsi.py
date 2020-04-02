0aae364d74d166471fe51b256aa5ae379644d817
Kingsley McDonald <kingsleymcdonald@outlook.com>
2020-04-03 00:27:19 +0100

    Add stub function definitions.

--- scsi/_scsi_linux.py
+++ scsi/_scsi_linux.py
@@ -1,2 +1,17 @@
 # This submodule should make use of the builtin `fcntl.ioctl` function
 # to interact with the SCSI Generic driver in Linux.
+
+import os
+
+
+def scsi_open(device_path: os.PathLike) -> int:
+    ...
+
+def scsi_read(device: int, command: bytes, amount: int, timeout: int) -> bytes:
+    ...
+
+def scsi_write(device: int, command: bytes, buffer: bytes) -> None:
+    ...
+
+def scsi_close(device: int) -> None:
+    ...
--- scsi/_scsi_windows.py
+++ scsi/_scsi_windows.py
@@ -1,3 +1,18 @@
 # This submodule should use the `ctypes` builtin module to interact
 # with the `kernel32.dll` library in the Windows API. We could then
 # use functions like `DeviceIoControl` to do whatever we need to do.
+
+import os
+
+
+def scsi_open(device_path: os.PathLike) -> int:
+    ...
+
+def scsi_read(device: int, command: bytes, amount: int, timeout: int) -> bytes:
+    ...
+
+def scsi_write(device: int, command: bytes, buffer: bytes) -> None:
+    ...
+
+def scsi_close(device: int) -> None:
+    ...
