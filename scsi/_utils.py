2b9f56fbfe4b6df4b5e695883959943554e3a9ce
Kingsley McDonald <kingsleymcdonald@outlook.com>
2020-04-03 06:05:54 +0100

    Add simple exception types and SCSIStatus enum to `utils.py`.

--- scsi/_utils.py
+++ scsi/_utils.py
@@ -1,4 +1,5 @@
 import ctypes as ct
+from enum import IntEnum
 
 # the type of `Structure` can be found in the `_ctypes` module, but we
 # cannot just import it because the `_ctypes` module does not export
@@ -15,3 +16,40 @@ class MetaTypedStructure(MetaStructure):
 
 class TypedStructure(ct.Structure, metaclass=MetaTypedStructure):
     pass
+
+
+class SCSIStatus(IntEnum):
+    GOOD = 0x00
+    CHECK_CONDITION = 0x02
+    CONDITION_MET = 0x04
+    BUSY = 0x08
+    INTERMEDIATE = 0x10  # obsolete
+    INTERMEDIATE_CONDITION_MET = 0x14  # obsolete
+    RESERVATION_CONFLICT = 0x18
+    COMMAND_TERMINATED = 0x22  # obsolete
+    TASK_SET_FULL = 0x28
+    ACA_ACTIVE = 0x30
+    TASK_ABORTED = 0x40
+
+    def raise_if_bad(self, message: str):
+        if self is not SCSIStatus.GOOD:
+            raise SCSIStatusError(self, message)
+
+
+class SCSIError(Exception):
+    """
+    A base class for SCSI-related errors.
+
+    Although inherited exception types are designed to be platform-
+    agnostic, their error messages may be platform-specific in order
+    to allow for better problem diagnosis.
+    """
+
+
+class SCSIStatusError(SCSIError):
+    def __init__(self, status: SCSIStatus, message: str):
+        self.status = status
+        self.message = message
+
+    def __str__(self):
+        return f"{self.status.name}: {self.message}"
